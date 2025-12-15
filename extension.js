55f27a18df123bcc7aa4450d6af80316c43ddf34
Girard Louis <lgirard@student.42lyon.fr>
2025-12-15 12:15:08 +0100

    save

--- extension.js
+++ extension.js
@@ -7,6 +7,8 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const GLib = imports.gi.GLib;
 const Gio = imports.gi.Gio;
 
+const { Debug } = Me.imports.utils.debug;
+
 const Me = ExtensionUtils.getCurrentExtension();
 
 let _indicator = null;
@@ -50,7 +52,36 @@ function enable() {
 	// automatically open login window on enable
 	_executeCookieCapture();
 	Connect.get_access_token(CLIENT_ID, CLIENT_SECRET, (token) => {
-		console.log(token);
+		if (token) {
+			const apiUrl = `https://api.intra.42.fr/v2/me`;
+			get_api_data(apiUrl, token, (data)=> {
+				Debug.logInfo(JSON.stringify(data));
+			});
+		}
+	});
+}
+
+function get_api_data(url, token, callback) {
+	let session = new Soup.Session();
+	let message = Soup.Message.new('GET', url);
+
+	message.request_headers.append('Authorization', `Bearer ${token}`);
+
+	session.queue_message(message, (sess, msg) => {
+		if (msg.status_code === 200) {
+			try {
+				let data = JSON.parse(msg.response_body.data);
+				Debug.logSuccess(`Received JSON data:\n${JSON.stringify(data, null, 2)}`);
+				callback(data);
+			} catch (e) {
+				Debug.logError(`Failed to parse JSON: ${e.message}`);
+			}
+		} else if (msg.status_code === 401) {
+			Debug.logError(`Unauthorized: Invalid or expired token.`);
+			callback(401);
+		} else {
+			Debug.logError(`HTTP error ${msg.status_code}: ${msg.reason_phrase}`);
+		}
 	});
 }
 
